b68a7edfba8f754720ce6deca0ebc0759d52f0ce
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-07-12 10:49:35 +0100

    CSS Fix

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/atom/Button";
 
 export default function Home() {
   return (
-    <div className="w-full flex justify-end p-16 fixed top-0">
+    <div className="w-min p-16 fixed top-0 right-0">
       <Button.Root className="w-12 h-12 rounded-full border-2 border-zinc-500" />
     </div>
   );
